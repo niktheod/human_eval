4b7c907272b1a0c6a8fe5f04e5b55708af721c48
Nikolaos Theodoridis <126592460+niktheod@users.noreply.github.com>
2025-05-19 12:24:55 +0100

    Update vqa_interface.py

--- vqa_interface.py
+++ vqa_interface.py
@@ -25,8 +25,8 @@ def get_drive_service():
         creds_dict = json.loads(st.secrets["google_credentials"])
         st.error(creds_dict)
         st.error(type(creds_dict))
-        creds = service_account.Credentials.from_service_account_info(
-            creds_dict, scopes=SCOPES)
+        # creds = service_account.Credentials.from_service_account_info(
+        #     creds_dict, scopes=SCOPES)
 
         service = build('drive', 'v3', credentials=creds)
         return service
